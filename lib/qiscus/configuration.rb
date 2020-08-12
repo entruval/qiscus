539aef326c50d9832a8bc7f7c5d80989abc12432
Tristanto <tristanto.kurniawan@gmail.com>
2020-08-12 10:07:39 +0700

    Add App ID

--- lib/qiscus/configuration.rb
+++ lib/qiscus/configuration.rb
@@ -1,6 +1,6 @@
 module Qiscus
   module Configuration
-    CONFIG = [:end_point, :api_version, :sdk_secret]
+    CONFIG = [:end_point, :api_version, :sdk_secret, :sdk_app_id]
 
     attr_accessor *CONFIG
 
